b4ee1f67675f7739d7491348c328f5b206451dcd
Solomon Sefa (sask) <kwabena81@yahoo.com>
2023-02-03 08:16:12 -0500

    complete initial setup

---
+++ book-microservice/src/main/java/com/edu/bookmicroservice/BookMicroserviceApplication.java
@@ -0,0 +1,17 @@
+package com.edu.bookmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableWebMvc
+@EnableEurekaClient
+//@EnableWebSecurity
+public class BookMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMicroserviceApplication.class, args);
+	}
+
+}
