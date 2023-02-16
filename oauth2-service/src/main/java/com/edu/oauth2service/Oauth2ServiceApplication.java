2307412235127964840e7bd4c5787e6827f889b4
Kwabena Solomon <kwabena81@yahoo.com>
2023-02-16 10:43:24 -0500

    further details defined

--- oauth2-service/src/main/java/com/edu/oauth2service/Oauth2ServiceApplication.java
+++ oauth2-service/src/main/java/com/edu/oauth2service/Oauth2ServiceApplication.java
@@ -2,8 +2,10 @@ package com.edu.oauth2service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Oauth2ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(Oauth2ServiceApplication.class, args);
--- oauth2-service/src/main/java/com/edu/oauth2service/WebSecurityConfig.java
+++ oauth2-service/src/main/java/com/edu/oauth2service/WebSecurityConfig.java
@@ -45,8 +45,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeRequests().anyRequest().permitAll();
+//                .csrf().disable()
+//                .authorizeRequests().anyRequest().permitAll();
+        .csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+
 //                .authorizeRequests().anyRequest().authenticated().and()
 //                .formLogin().loginPage("/login").permitAll();
     }
